f116ea7e19c06509381d0b479f6351d1b2b285a0
unknown <kevinrai1225@gmail.com>
2026-04-13 14:28:07 +0545

    hi

---
+++ pythondemo.py
@@ -0,0 +1,32 @@
+import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist))
+# list
+age=[20]
+print(age)
+print(id(age))
+print(type(age))
+
+# complex
+age=20J
+print(age)
+print(id(age))
+print(type(age))
+
+# tuple
+age=20,
+print(age)
+print(id(age))
+print(type(age))
+
+# set
+age={*()}
+print(age)
+print(id(age))
+print(type(age))
+
+#dict
+age=dict
+print(age)
+print(id(age))
+print(type(age))
